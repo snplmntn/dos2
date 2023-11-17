3e47e3378ac045776af68f40a4af9129a0991c07
snplmntn <124994202+snplmntn@users.noreply.github.com>
2023-11-17 09:59:50 +0800

    Bug Fix
     Error: res.status is not a function on AliveController

--- controller/AliveController.js
+++ controller/AliveController.js
@@ -1,13 +1,13 @@
-const alive = async (res) => {
+const alive = (req, res) => {
   const timestamp = Date.now();
-
   const date = new Date(timestamp);
   const formattedDate = date.toLocaleString();
+
   try {
-    return res.status(200).json({ message: "I AM ALIVE", Date: formattedDate });
+    res.status(200).json({ message: "I AM ALIVE", Date: formattedDate });
   } catch (err) {
-    console.error("I AM DEAD", formattedDate);
-    return res.status(500).json({ message: "Internal Server Error", err });
+    console.error("I AM DEAD", formattedDate, err);
+    res.status(500).json({ message: "Internal Server Error", err });
   }
 };
 
