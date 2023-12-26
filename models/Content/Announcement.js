b1acb79c39ec3d14798c2e7dd7c8d5172f55357e
snplmntn <124994202+snplmntn@users.noreply.github.com>
2023-12-26 22:09:12 +0800

    Add files via upload

--- models/Content/Announcement.js
+++ models/Content/Announcement.js
@@ -19,7 +19,7 @@ const AnnouncementSchema = new mongoose.Schema({
   },
   category: {
     type: Number,
-    enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
     default: 0,
   },
   dateCreated: {
